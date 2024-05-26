2f88c0f3f87b2752b84f6c077538bb1f3244d816
Musa Molla <musamolla3131@gmail.com>
2024-05-27 01:56:05 +0600

    spealling

--- src/scripts/MdNazmusIslam.bat
+++ src/scripts/MdNazmusIslam.bat
@@ -1,7 +1,7 @@
 @setlocal DisableDelayedExpansion
 @echo off
 
-set "userName=Md Nazmus Islam"
+set "userName=Md Nazmul Islam"
 
 for /f "tokens=2 delims==" %%A in ('wmic os get Caption /value') do (for /f "tokens=*" %%B in ("%%A") do set "OS=%%B")
 for /f "tokens=2 delims==" %%A in ('wmic path softwarelicensingservice get OA3xOriginalProductKey /value') do ( set "P=%%A")
